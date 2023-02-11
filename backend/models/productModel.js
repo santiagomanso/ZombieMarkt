b6c350c4aca880277a2781030b8eb4a8ec74a9a0
santiagomanso <santiagomanso@web.de>
2023-02-11 13:41:32 +0100

    🔨 created models

---
+++ backend/models/productModel.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose'
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  sku: {
+    type: Number,
+    required: true,
+  },
+  ean: {
+    type: Number,
+    required: true,
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    default: 3,
+  },
+  numReviews: { type: Number, required: false, default: 0 },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+})
+
+const productModel = mongoose.model('product', productSchema)
+
+export default productModel
