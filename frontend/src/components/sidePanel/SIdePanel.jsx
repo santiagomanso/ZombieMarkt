2fb0a305cfb7c6e4c370a64a028c40fe77428038
santiagomanso <santiagomanso@web.de>
2023-02-11 13:47:11 +0100

    🔨 created component

---
+++ frontend/src/components/sidePanel/SIdePanel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SIdePanel = () => {
+  return (
+    <aside className='hidden lg:block row-span-full bg-blue-300 h-full w-72'>
+      SIdePanel
+    </aside>
+  )
+}
+
+export default SIdePanel
