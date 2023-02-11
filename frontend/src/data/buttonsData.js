f8b5e529674ea3624b9372521f0b21ffd018a20b
santiagomanso <santiagomanso@web.de>
2023-02-11 13:47:30 +0100

    🗃️ data obj for navbar

---
+++ frontend/src/data/buttonsData.js
@@ -0,0 +1,22 @@
+export const buttonsData = [
+  {
+    id: 1,
+    title: 'Inbound inventory',
+    path: '/inbound',
+  },
+  {
+    id: 2,
+    title: 'Outbound inventory',
+    path: '/outbound',
+  },
+  {
+    id: 3,
+    title: 'Inventory Check',
+    path: '/check',
+  },
+  {
+    id: 4,
+    title: 'Backup shelves',
+    path: '/shelves',
+  },
+]
