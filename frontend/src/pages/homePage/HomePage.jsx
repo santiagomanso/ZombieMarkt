6bbf53e7c8c64fe731c75f8d21385696a7de74a6
santiagomanso <santiagomanso@web.de>
2023-02-11 13:49:48 +0100

    🔨 created home page

---
+++ frontend/src/pages/homePage/HomePage.jsx
@@ -0,0 +1,26 @@
+import { buttonsData } from '../../data/buttonsData'
+import { useNavigate } from 'react-router-dom'
+
+const HomePage = () => {
+  const navigate = useNavigate()
+  return (
+    <section className='ml-0 sm:ml-36 mr-0 sm:mr-36 flex flex-col sm:flex-row justify-center items-center flex-wrap'>
+      {buttonsData.map((button) => {
+        return (
+          <div
+            key={button.id}
+            className='m-2 sm:m-4 h-16 sm:h-64 w-64 bg-stone-200 rounded-md'
+          >
+            <button
+              onClick={() => navigate(button.path)}
+              className='font-bold text-xl h-full w-full'
+            >
+              {button.title}
+            </button>
+          </div>
+        )
+      })}
+    </section>
+  )
+}
+export default HomePage
