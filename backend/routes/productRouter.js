3e1cc78ccc9547ad24d95eef412c45304590ff0d
santiagomanso <santiagomanso@web.de>
2023-02-12 20:58:15 +0100

    🚀 new endpoint on router POST

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -3,11 +3,17 @@ import {
   getAllProducts,
   getProductsByEAN,
   getProductsByName,
+  postNewProduct,
 } from '../controllers/productController.js'
 
 const productRouter = express.Router()
+
+//NOTE GET
 productRouter.get('/all', getAllProducts)
 productRouter.get('/name/:name', getProductsByName)
 productRouter.get('/ean/:ean', getProductsByEAN)
+
+//NOTE POST
+productRouter.post('/create', postNewProduct)
 
 export default productRouter
