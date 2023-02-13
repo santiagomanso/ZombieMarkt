859974ee21b9679d3faca0d7f187a48b4495d0e5
santiagomanso <santiagomanso@web.de>
2023-02-13 03:04:10 +0100

    🔥 create product, auto focus, error handling

--- frontend/src/pages/create/CreatePage.jsx
+++ frontend/src/pages/create/CreatePage.jsx
@@ -1,24 +1,80 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useRef, useState } from 'react'
+import FloatingMsg from '../../components/floatingMsg/FloatingMsg'
 
 const CreatePage = () => {
   const [newProduct, setNewProduct] = useState('')
+  const [response, setResponse] = useState('')
   const [error, setError] = useState('')
+  const nameRef = useRef(null)
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
+    createProduct()
   }
 
   const handleChange = (e) => {
     setNewProduct({ ...newProduct, [e.target.name]: e.target.value })
   }
 
-  const createProduct = async () => {}
+  useEffect(() => {
+    nameRef.current.focus() //NOTE - auto focus name on component mount ;)
+    //NOTE - reset error on tablets/phones due to insuficient space
+    if (window.innerWidth <= 1024 && error.length > 1) {
+      setTimeout(() => {
+        setError('')
+      }, 4500)
+    }
+  }, [error])
+
+  const createProduct = async () => {
+    const myHeaders = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    }
+
+    const data = {
+      name: newProduct.name,
+      sku: newProduct.sku,
+      ean: newProduct.ean,
+      countInStock: newProduct.countInStock,
+      image: newProduct.image,
+      category: newProduct.category,
+    }
+
+    try {
+      const res = await axios.post(
+        'http://localhost:5500/api/products/create',
+        data,
+        { headers: myHeaders },
+      )
+
+      setResponse(res.data)
+    } catch (error) {
+      console.log(error.response.data.message)
+      setError(error.response.data.message)
+    }
+  }
 
   return (
-    <div className={` lg:flex lg:flex-col lg:justify-center px-0 lg:px-14`}>
-      <div
-        className={`bg-white via-slate-300 group py-0 sm:py-4 px-0 sm:px-4 overflow-auto rounded-lg  lg:h-[600px] lg:flex duration-500 outline outline-1 outline-gray-300 shadow-md grid placeis mt-5 lg:mt-20`}
+    <main className={` lg:flex lg:flex-col lg:justify-center px-0 lg:px-14`}>
+      <section
+        className={`bg-white via-slate-300 group py-0 sm:py-4 px-0 sm:px-4 rounded-lg  lg:h-[600px] lg:flex duration-500 outline outline-1 outline-gray-300 shadow-md grid placeis mt-5 lg:mt-20 relative`}
       >
+        {error ? (
+          <FloatingMsg
+            msg={error}
+            icon='fa-circle-exclamation'
+            opt='text-red-700 bg-rose-100 px-3 py-2 rounded outline outline-1 outline-red-500 top-[10%] left-[50%] lg:top-[10%] z-10 lg:left-[18%] -translate-x-[50%] flex flex-col lg:flex-row items-center lg:items-baseline gap-1 w-3/4  lg:w-auto'
+          />
+        ) : error.length === 0 ? (
+          ''
+        ) : (
+          <FloatingMsg
+            msg='product created successfully'
+            icon='fa-circle-check'
+            opt='text-green-700 bg-emerald-100 px-3 py-2 rounded outline outline-1 outline-green-500 top-[10%] left-[50%] lg:top-[10%] z-10 lg:left-[18%] -translate-x-[50%] flex flex-col lg:flex-row items-center lg:items-baseline gap-1 w-3/4  lg:w-auto'
+          />
+        )}
         <div className='hidden lg:flex group w-1/3  flex-col gap-y-3 justify-center items-center'>
           <span className='group-hover:rotate-0 duration-700 text-5xl -rotate-3 text-gray-600 font-semibold'>
             Create Product
@@ -29,7 +85,7 @@ const CreatePage = () => {
           <i className='fa-solid fa-square-plus text-6xl -rotate-[45deg] group-hover:rotate-0 duration-1000 text-gray-500'></i>
         </div>
         <div className='lg:h-full lg:w-2/3 p-2 lg:p-7'>
-          <form
+          <article
             className='grid grid-cols-1 lg:grid-cols-3 lg:gap-8'
             onSubmit={handleSubmit}
           >
@@ -38,6 +94,7 @@ const CreatePage = () => {
               <input
                 className='bg-gray-100 outline outline-1 outline-gray-300'
                 type='text'
+                ref={nameRef}
                 name='name'
                 id='name'
                 onChange={handleChange}
@@ -68,18 +125,8 @@ const CreatePage = () => {
               <input
                 className='bg-gray-100 outline outline-1 outline-gray-300'
                 type='number'
-                name='stock'
+                name='countInStock'
                 id='stock'
-                onChange={handleChange}
-              />
-            </div>
-            <div className='hidden lg:flex flex-col col-span-3 lg:col-span-1 w-full'>
-              <label htmlFor='rating'>Initial Rating</label>
-              <input
-                className='bg-gray-100 outline outline-1 outline-gray-300'
-                type='number'
-                name='rating'
-                id='rating'
                 onChange={handleChange}
               />
             </div>
@@ -103,13 +150,23 @@ const CreatePage = () => {
                 <option value='sweet'>sweet</option>
                 <option value='icecream'>icecream</option>
                 <option value='books'>books</option>
-                <option value='computer'>computer</option>
+                <option value='hygiene'>hygiene</option>
               </select>
             </div>
-            <div className='flex mt-5 flex-col h-40  bg-blue-400 lg:h-[150px]  col-span-3 relative '>
+            <div className='flex flex-col col-span-3 lg:col-span-1 w-full'>
+              <label htmlFor='image'>Image</label>
+              <input
+                className='bg-gray-100 outline outline-1 outline-gray-300'
+                type='text'
+                name='image'
+                id='image'
+                onChange={handleChange}
+              />
+            </div>
+            <div className='flex mt-5 flex-col h-40  bg-blue-400 lg:h-[150px]  col-span-3 relative rounded z-0'>
               <label
-                htmlFor='image'
-                className='bg-gray-100 w-full h-full outline-2 outline-dashed outline-gray-300 cursor-pointer'
+                htmlFor='imageFile'
+                className='bg-gray-100 w-full h-full outline-2 outline-dashed outline-gray-300 cursor-pointer rounded'
               >
                 <span className='absolute top-[50%] lg:top-[25%] -translate-y-[50%]  left-[20%] lg:left-[50%] lg:-translate-x-[50%] lg:text-3xl text-gray-600'>
                   Upload picture or drag & drop
@@ -118,8 +175,8 @@ const CreatePage = () => {
               <input
                 className='hidden'
                 type='file'
-                name='image'
-                id='image'
+                name='imageFile'
+                id='imageFile'
                 accept='.jpg, .png, .jpeg'
                 onChange={handleChange}
               />
@@ -127,19 +184,21 @@ const CreatePage = () => {
             </div>
             <div className='flex justify-center col-span-1 self-center'>
               <button
-                type='submit'
-                className={` hidden  sm:block bg-slate-400 rounded-md cursor-pointer    w-full h-12                  
+                onClick={handleSubmit}
+                type='button'
+                className={` hidden  sm:block bg-gray-500/90 rounded-md cursor-pointer    w-full h-12                  
                 text-white font-bold text-lg`}
               >
                 Create Product
               </button>
             </div>
-          </form>
+          </article>
         </div>
-      </div>
+      </section>
 
       <div className='flex justify-center'>
         <button
+          onClick={handleSubmit}
           type='button'
           className={` bottom-4 fixed sm:hidden bg-slate-400 rounded-md cursor-pointer w-5/6 h-12                  
                 text-white font-bold text-lg`}
@@ -147,7 +206,7 @@ const CreatePage = () => {
           Create product
         </button>
       </div>
-    </div>
+    </main>
   )
 }
 
