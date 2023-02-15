4a121c9b7a928e7c043173f1108a1130622bcdf0
santiagomanso <santiagomanso@web.de>
2023-02-15 23:58:00 +0100

    ➕ added cloudinary

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,4 @@
+import { v2 as cloudinary } from 'cloudinary'
 import productModel from '../models/productModel.js'
 
 export const getAllProducts = async (req, res) => {
@@ -15,6 +16,7 @@ export const getAllProducts = async (req, res) => {
   }
 }
 
+//NOTE GET BY NAME
 export const getProductsByName = async (req, res) => {
   const { name } = req.params
   try {
@@ -25,6 +27,7 @@ export const getProductsByName = async (req, res) => {
   }
 }
 
+//NOTE GET BY EAN
 export const getProductsByEAN = async (req, res) => {
   const { ean } = req.params
   console.log('EAN: ', ean)
@@ -36,63 +39,90 @@ export const getProductsByEAN = async (req, res) => {
   }
 }
 
-//NOTE 1nd step
-export const postImage = async (req, res) => {
-  res.json(req.file)
-}
-
 //NOTE POST NEW PRODUCT
 export const postNewProduct = async (req, res) => {
   //destructure fields that are mandatory
-  const { name, ean, sku, category, image, countInStock } = req.body
-  if (!name && !ean && !sku && !category && !image && !countInStock) {
-    return res.status(500).json({
-      message: 'error empty field/s detected',
+  // console.log('asdadas', req.file)
+
+  const { name, ean, sku, category, countInStock, price, rating, numReviews } =
+    req.body
+
+  // if (!name || !ean || !sku || !category || !countInStock || !price) {
+  //   return res.status(500).json({
+  //     message: 'error empty field/s detected',
+  //   })
+  // }
+
+  let imageUrl
+  if (req.file) {
+    //TODO search more adecuate way of dealing with undefined
+    //NOTE OBJECTO COMO RESPUESTA>
+    imageUrl = await cloudinary.uploader.upload(req.file.path, {
+      folder: 'productImages',
     })
   }
-  try {
-    const existingProduct = await productModel.findOne({ ean: ean })
-    if (existingProduct) {
-      console.log('error product already exists')
-      res.status(501).json({ message: 'error: product already exists' })
-    } else {
-      const newProduct = new productModel({
-        name: name,
-        sku: sku,
-        ean: ean,
-        countInStock: countInStock,
-        rating: req.body.rating,
-        numReviews: req.body.numReviews,
-        image: image,
-        category: category,
-      })
-      try {
-        const savedProduct = await newProduct.save()
-        // console.log(savedProduct)
-        res.status(201).json({
-          msg: 'Product created successfully',
-          product: savedProduct,
-        })
-      } catch (error) {
-        console.log('error server')
-        res.status(503).json({
-          msg: 'error inner catch',
-        })
-      }
-    }
-  } catch (error) {
-    console.log('error undefined')
-    res.status().json({
-      message: error,
-    })
-  }
+  if (imageUrl) imageUrl = imageUrl.url
+  console.log('imageUrl', imageUrl)
+
+  // try {
+  //   const existingProduct = await productModel.findOne({ ean: ean })
+  //   if (existingProduct) {
+  //     console.log('error product already exists')
+  //     res.status(501).json({ message: 'error: product already exists' })
+  //   } else {
+  //     const newProduct = new productModel({
+  //       name: name,
+  //       sku: sku,
+  //       ean: ean,
+  //       countInStock: countInStock,
+  //       price: price,
+  //       rating: rating,
+  //       numReviews: numReviews,
+  //       image: imageUrl,
+  //       category: category,
+  //     })
+  //     console.log('newProduct', newProduct)
+  //     try {
+  //       const savedProduct = await newProduct.save()
+  //       // console.log(savedProduct)
+  //       res.status(201).json({
+  //         msg: 'Product created successfully',
+  //         product: savedProduct,
+  //       })
+  //     } catch (error) {
+  //       console.log('error server')
+  //       console.log(error)
+  //       res.status(503).json({
+  //         msg: error,
+  //       })
+  //     }
+  //   }
+  // } catch (error) {
+  //   console.log('error undefined')
+  //   res.status().json({
+  //     message: error,
+  //   })
+  // }
 }
 
+//NOTE UPDATE PRODUCT
 export const updateProduct = async (req, res) => {
   const { _id } = req.params
 
-  const { name, ean, sku, countInStock, rating, numReviews, image, category } =
+  const { name, ean, sku, countInStock, rating, numReviews, category } =
     req.body
+
+  // console.log('req.file', req.file)
+  let imageUrl
+  if (req.file) {
+    //TODO search more adecuate way of dealing with undefined
+    //NOTE OBJECTO COMO RESPUESTA>
+    imageUrl = await cloudinary.uploader.upload(req.file.path, {
+      folder: 'productImages',
+    })
+  }
+  if (imageUrl) imageUrl = imageUrl.url
+  // console.log('imageUrl', imageUrl)
 
   const newProduct = {}
   if (name) newProduct.name = name
@@ -101,8 +131,10 @@ export const updateProduct = async (req, res) => {
   if (countInStock) newProduct.countInStock = countInStock
   if (rating) newProduct.rating = rating
   if (numReviews) newProduct.numReviews = numReviews
-  if (image) newProduct.image = image
   if (category) newProduct.category = category
+  if (imageUrl) newProduct.image = imageUrl
+
+  console.log('newProduct', newProduct)
 
   try {
     let product = await productModel.findById(_id)
@@ -124,6 +156,22 @@ export const updateProduct = async (req, res) => {
   } catch (error) {
     res.status(500).json({
       msg: 'Internal error',
+    })
+  }
+}
+
+//NOTE DELETE PRODUCT
+export const deleteProduct = async (req, res) => {
+  const { _id } = req.params
+
+  try {
+    await productModel.deleteOne({ _id: _id })
+    res.status(200).json({
+      msg: 'Product was successfully deleted',
+    })
+  } catch (error) {
+    res.status(401).json({
+      msg: 'There was an error while trying to delete',
     })
   }
 }
