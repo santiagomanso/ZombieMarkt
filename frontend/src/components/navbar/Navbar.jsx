5bee5fe26132d9278f20ea611f66a2ea23c8eec1
santiagomanso <santiagomanso@web.de>
2023-02-16 16:36:28 +0100

    ✨ 💬 switched name based on path

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,5 +1,34 @@
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+
 const Navbar = () => {
+  const [title, setTitle] = useState('')
+
+  const location = useLocation()
+  useEffect(() => {
+    changeTitle()
+  }, [location])
+
+  const changeTitle = () => {
+    switch (true) {
+      case location.pathname === '/create': {
+        return setTitle('create_product')
+      }
+
+      case location.pathname === '/update': {
+        return setTitle('update_product')
+      }
+
+      case location.pathname === '/delete': {
+        return setTitle('delete_product')
+      }
+
+      default:
+        return setTitle('Inventory_Manager')
+    }
+  }
+
+  console.log('location', location)
   return (
     <nav className='bg-white p-5 sm:px-14 sm:py-12 flex justify-between items-center h-16 text-black relative shadow'>
       <Link
@@ -8,7 +37,7 @@ const Navbar = () => {
       >
         Home
       </Link>
-      <h1 className='text-xl md:text-3xl font-bold'>inventory-manager</h1>
+      <h1 className='text-xl md:text-3xl font-bold'>{title}</h1>
       <span className='bg-gray-500 py-1 sm:py-4  px-2 sm:px-7  text-white rounded-lg font-bold'>
         Logout
       </span>
