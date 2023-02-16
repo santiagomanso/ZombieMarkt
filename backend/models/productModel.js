4098e1bbfc53b978acdf4821884360eeda81995a
santiagomanso <santiagomanso@web.de>
2023-02-16 15:28:34 +0100

    ✨ added random default variable

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,5 +1,17 @@
 import mongoose from 'mongoose'
 
+const randomShelf = () => {
+  const shelves = ['A1', 'B2', 'C1', 'A2', 'C2', 'B3', 'A3', 'B1', 'C3']
+  return shelves[Math.floor(Math.random() * shelves.length)]
+}
+
+const randomPrice = () => {
+  const prices = [
+    1.29, 0.84, 1.54, 2.21, 1, 2, 2.43, 6.12, 1.9, 2.78, 1.95, 0.43, 0.55, 0.4,
+  ]
+  return prices[Math.floor(Math.random() * prices.length)]
+}
+
 const productSchema = mongoose.Schema({
   name: {
     type: String,
@@ -22,6 +34,7 @@ const productSchema = mongoose.Schema({
   price: {
     type: Number,
     required: true,
+    default: randomPrice(),
   },
   rating: {
     type: Number,
@@ -41,6 +54,16 @@ const productSchema = mongoose.Schema({
   category: {
     type: String,
     required: true,
+  },
+  shelf: {
+    type: String,
+    required: false,
+    default: randomShelf(),
+  },
+  backstock: {
+    type: String,
+    required: false,
+    default: randomShelf(),
   },
 })
 
