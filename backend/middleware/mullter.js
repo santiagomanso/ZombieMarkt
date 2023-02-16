d5bcd2a2f6238c698b652ae05014e7aa750c7f74
santiagomanso <santiagomanso@web.de>
2023-02-16 15:25:35 +0100

    ➖ 💬 removed console.logs

--- backend/middleware/mullter.js
+++ backend/middleware/mullter.js
@@ -4,10 +4,12 @@ import path from 'path'
 const multerUpload = multer({
   storage: multer.diskStorage({}),
   fileFilter: function (req, file, cb) {
+    console.log('MULTER file')
     let extension = path.extname(file.originalname)
     if (extension !== '.jpg' && extension !== '.jpeg' && extension !== '.png') {
       cb(new Error('File extension not supported'), false)
     }
+    console.log('NOOOOO')
     cb(null, true)
   },
 })
