22ed1c199cb24aaeaee468951b78d2386fc649f7
santiagomanso <santiagomanso@web.de>
2023-02-16 15:27:51 +0100

    🚧 🔧 reworked req, no more multer

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -41,68 +41,73 @@ export const getProductsByEAN = async (req, res) => {
 
 //NOTE POST NEW PRODUCT
 export const postNewProduct = async (req, res) => {
-  //destructure fields that are mandatory
-  // console.log('asdadas', req.file)
+  const {
+    name,
+    ean,
+    sku,
+    category,
+    countInStock,
+    price,
+    image,
+    shelf,
+    backstock,
+  } = req.body
 
-  const { name, ean, sku, category, countInStock, price, rating, numReviews } =
-    req.body
-
-  // if (!name || !ean || !sku || !category || !countInStock || !price) {
-  //   return res.status(500).json({
-  //     message: 'error empty field/s detected',
-  //   })
-  // }
-
-  let imageUrl
-  if (req.file) {
-    //TODO search more adecuate way of dealing with undefined
-    //NOTE OBJECTO COMO RESPUESTA>
-    imageUrl = await cloudinary.uploader.upload(req.file.path, {
-      folder: 'productImages',
+  if (!name || !ean || !sku || !category || !countInStock || !price) {
+    return res.status(500).json({
+      message: 'error empty field/s detected',
     })
   }
-  if (imageUrl) imageUrl = imageUrl.url
-  console.log('imageUrl', imageUrl)
 
-  // try {
-  //   const existingProduct = await productModel.findOne({ ean: ean })
-  //   if (existingProduct) {
-  //     console.log('error product already exists')
-  //     res.status(501).json({ message: 'error: product already exists' })
-  //   } else {
-  //     const newProduct = new productModel({
-  //       name: name,
-  //       sku: sku,
-  //       ean: ean,
-  //       countInStock: countInStock,
-  //       price: price,
-  //       rating: rating,
-  //       numReviews: numReviews,
-  //       image: imageUrl,
-  //       category: category,
-  //     })
-  //     console.log('newProduct', newProduct)
-  //     try {
-  //       const savedProduct = await newProduct.save()
-  //       // console.log(savedProduct)
-  //       res.status(201).json({
-  //         msg: 'Product created successfully',
-  //         product: savedProduct,
-  //       })
-  //     } catch (error) {
-  //       console.log('error server')
-  //       console.log(error)
-  //       res.status(503).json({
-  //         msg: error,
-  //       })
-  //     }
-  //   }
-  // } catch (error) {
-  //   console.log('error undefined')
-  //   res.status().json({
-  //     message: error,
+  // let imageUrl
+  // if (req.file) {
+  //   imageUrl = await cloudinary.uploader.upload(req.file.path, {
+  //     folder: 'productImages',
   //   })
   // }
+  // if (imageUrl) imageUrl = imageUrl.url
+  // console.log('imageUrl', imageUrl)
+  console.log('req.body', req.body)
+
+  try {
+    const existingProduct = await productModel.findOne({ ean: ean })
+    if (existingProduct) {
+      console.log('error product already exists')
+      res.status(501).json({ message: 'error: product already exists' })
+    } else {
+      const newProduct = new productModel({
+        name: name,
+        sku: sku,
+        ean: ean,
+        countInStock: countInStock,
+        price: price,
+        image: image,
+        category: category,
+        shelf: shelf,
+        backstock: backstock,
+      })
+      // console.log('newProduct', newProduct)
+      try {
+        const savedProduct = await newProduct.save()
+        // console.log(savedProduct)
+        res.status(201).json({
+          msg: 'Product created successfully',
+          product: savedProduct,
+        })
+      } catch (error) {
+        console.log('error server')
+        // console.log(error)
+        res.status(503).json({
+          msg: error,
+        })
+      }
+    }
+  } catch (error) {
+    console.log('error undefined')
+    res.status().json({
+      message: error,
+    })
+  }
 }
 
 //NOTE UPDATE PRODUCT
--- backend/middleware/mullter.js
+++ backend/middleware/mullter.js
@@ -4,14 +4,15 @@ import path from 'path'
 const multerUpload = multer({
   storage: multer.diskStorage({}),
   fileFilter: function (req, file, cb) {
-    console.log('MULTER file')
+    console.log('MULTER', file)
     let extension = path.extname(file.originalname)
     if (extension !== '.jpg' && extension !== '.jpeg' && extension !== '.png') {
-      cb(new Error('File extension not supported'), false)
+      // cb(new Error('File extension not supported'), false)
+      console.log('extension checked')
+    } else {
+      console.log('all good')
+      cb(null, true)
     }
-    console.log('NOOOOO')
-    cb(null, true)
   },
 })
-
 export { multerUpload }
