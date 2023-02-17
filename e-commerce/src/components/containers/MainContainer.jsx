90292bcd01920f32864f8a7247da1d8260284bc9
santiagomanso <santiagomanso@web.de>
2023-02-17 17:22:04 +0100

    ⚰️ removed dead code

--- e-commerce/src/components/containers/MainContainer.jsx
+++
@@ -1,9 +0,0 @@
-const MainContainer = ({ children }) => {
-  return (
-    <div className='h-screen bg-gradient-to-br from-amber-100 to-slate-900'>
-      {children}
-    </div>
-  )
-}
-
-export default MainContainer
