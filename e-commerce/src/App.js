5a98f804aeef377e7b9f300dab6536a53a3b7fb4
santiagomanso <santiagomanso@web.de>
2023-02-17 17:20:55 +0100

    🚚 🔧 renamed / added route

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,16 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import MainContainer from './components/containers/MainContainer'
+import AppContainer from './components/containers/AppContainer'
 import Navbar from './components/navbar/Navbar'
+import Categories from './pages/Categories'
 import HomePage from './pages/HomePage'
 const App = () => {
   return (
     <BrowserRouter>
-      <MainContainer>
+      <AppContainer>
         <Navbar />
         <Routes>
           <Route path='/' element={<HomePage />} />
+          <Route path='/categories' element={<Categories />} />
         </Routes>
-      </MainContainer>
+      </AppContainer>
     </BrowserRouter>
   )
 }
