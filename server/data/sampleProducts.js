c54b899d419ff9735962dad400850887dc61da51
santiagomanso <santiagomanso@web.de>
2023-02-20 16:41:50 +0100

    🗃️ sample data, see "seeder.js"

--- server/data/sampleProducts.js
+++ server/data/sampleProducts.js
@@ -1,6 +1,6 @@
 export const sampleProducts = [
   {
-    name: 'coca cola 1lt',
+    name: 'coca cola 333lt',
     sku: 23423425,
     ean: 11231434255,
     countInStock: 20,
@@ -8,44 +8,49 @@ export const sampleProducts = [
     rating: 4.5,
     numReviews: 2,
     image:
-      'https://cdn.shopify.com/s/files/1/0559/5676/6914/products/MexicanCocaCola.jpg?v=1675434658',
-    category: 'beverages',
+      'https://i.ibb.co/6W5kxqy/Mexican-Coca-Cola-removebg-preview-removebg-preview.png',
+    categoryName: 'beverages',
+    category: '63f35eef60bb4581cd3a7a54',
   },
+  // {
+  //   name: 'lays salt & vinegar 120g',
+  //   sku: 2431313,
+  //   ean: 123123435,
+  //   countInStock: 3,
+  //   price: 1.21,
+  //   rating: 3.5,
+  //   numReviews: 1,
+  //   image:
+  //     'https://lays.de/images/default-source/default-album/products/packshots/1.-core-nutri/03-core-saltvinegar.png?sfvrsn=9d67efa5_2',
+  // categoryName:'beverages',//
+  // category: 'snacks',
+  // },
   {
-    name: 'lays salt & vinegar 120g',
-    sku: 2431313,
-    ean: 123123435,
-    countInStock: 3,
-    price: 1.21,
-    rating: 3.5,
-    numReviews: 1,
-    image:
-      'https://lays.de/images/default-source/default-album/products/packshots/1.-core-nutri/03-core-saltvinegar.png?sfvrsn=9d67efa5_2',
-    category: 'snacks',
-  },
-  {
-    name: 'pepsi 333 dose',
+    name: 'pepsi cola 333 dose',
     sku: 132343,
     ean: 241234123,
     countInStock: 23,
     price: 0.79,
     rating: 2,
     numReviews: 4,
-    image: 'https://m.media-amazon.com/images/I/61LXtgkFFUL._SL1500_.jpg',
-    category: 'beverages',
-  },
-  {
-    name: 'ja! gebäck stangen mit kässe',
-    sku: 213134,
-    ean: 1231324121,
-    countInStock: 45,
-    price: 1.11,
-    rating: 4.5,
-    numReviews: 5,
     image:
-      'https://lh3.googleusercontent.com/spp/AHlUXW0xTZ0KH5CExpTRBA8mrOUd4vCeWKtk6-yrYFJzIlKK7xvH5An9lMIQUEiJmVF_6A7r38jVexsQm8bG8_ReqAyOlnMsIPkHlhnufgspwpSfUz70Tp_ISqHXtwAHdrjm736966zj31o_fAw3G2Er_EQ6xqbLxGB1IV66XvlJagpPTKZWXsBXzbEQ9QdWhHi1MSRAtILHrQ=s512-rw-pd-pc0x00ffffff',
-    category: 'snacks',
+      'https://i.ibb.co/D8PpYbL/61-LXtgk-FFUL-SL1500-removebg-preview-removebg-preview.png',
+    categoryName: 'beverages',
+    category: '63f35eef60bb4581cd3a7a54',
   },
+  // {
+  //   name: 'ja! gebäck stangen mit kässe',
+  //   sku: 213134,
+  //   ean: 1231324121,
+  //   countInStock: 45,
+  //   price: 1.11,
+  //   rating: 4.5,
+  //   numReviews: 5,
+  //   image:
+  //     'https://lh3.googleusercontent.com/spp/AHlUXW0xTZ0KH5CExpTRBA8mrOUd4vCeWKtk6-yrYFJzIlKK7xvH5An9lMIQUEiJmVF_6A7r38jVexsQm8bG8_ReqAyOlnMsIPkHlhnufgspwpSfUz70Tp_ISqHXtwAHdrjm736966zj31o_fAw3G2Er_EQ6xqbLxGB1IV66XvlJagpPTKZWXsBXzbEQ9QdWhHi1MSRAtILHrQ=s512-rw-pd-pc0x00ffffff',
+  // categoryName:'beverages',//
+  // category: 'snacks',
+  // },
   {
     name: 'pepsi cola dose',
     sku: 1234121253,
@@ -54,9 +59,9 @@ export const sampleProducts = [
     price: 0.81,
     rating: 3.5,
     numReviews: 11,
-    image:
-      'https://www.sweet24.de/out/pictures/generated/product/1/540_340_90/pepsi_cola_033l_dose.jpg',
-    category: 'beverages',
+    image: 'https://i.ibb.co/k0LBZ58/image-removebg-preview.png',
+    categoryName: 'beverages',
+    category: '63f35eef60bb4581cd3a7a54',
   },
   {
     name: 'fritz cola 333ml',
@@ -68,6 +73,7 @@ export const sampleProducts = [
     numReviews: 8,
     image:
       'https://getraenkeservice-muenchen.com/wp-content/uploads/2018/01/fritz-kola-ohne-zucker-575x575.png',
-    category: 'beverages',
+    categoryName: 'beverages',
+    category: '63f35eef60bb4581cd3a7a54',
   },
 ]
