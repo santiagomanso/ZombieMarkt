495cb8a6c0051837db8fd9f595da23d4baf3c2c9
santiagomanso <santiagomanso@web.de>
2023-02-20 16:31:02 +0100

    🚚 added route, reworked component folder names

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AppContainer from './components/containers/AppContainer'
 import Navbar from './components/navbar/Navbar'
-import Categories from './pages/Categories'
-import HomePage from './pages/HomePage'
+import ProductsPage from './pages/Products'
+import HomePage from './pages/Home'
+import Details from './pages/Details'
 const App = () => {
   return (
     <BrowserRouter>
@@ -10,7 +11,8 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path='/' element={<HomePage />} />
-          <Route path='/categories' element={<Categories />} />
+          <Route path='/products/:category' element={<ProductsPage />} />
+          <Route path='/products/details/:_id' element={<Details />} />
         </Routes>
       </AppContainer>
     </BrowserRouter>
