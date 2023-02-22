d541bc156088403abcd13348680dec45e1448cfb
santiagomanso <santiagomanso@web.de>
2023-02-22 16:41:35 +0100

    🔧 implemented user router

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import productRouter from './routes/productRouter.js'
 import connectDB from './config/connectDB.js'
 import cloudinaryConfig from './config/cloudinary.js'
 import categoriesRouter from './routes/categoriesRouter.js'
+import userRouter from './routes/userRouter.js'
 dotenv.config()
 
 const app = express()
@@ -25,7 +26,8 @@ const addMiddlewares = () => {
     origin: 'http://localhost:3000',
     credentials: true,
   }
-  app.use(cors(corsOption))
+  // app.use(cors(corsOption))
+  app.use(cors())
   app.use(express.json())
   app.use(
     express.urlencoded({
@@ -39,6 +41,7 @@ const addMiddlewares = () => {
 const loadRoutes = () => {
   app.use('/api/products', productRouter)
   app.use('/api/categories', categoriesRouter)
+  app.use('/api/users', userRouter)
 }
 
 ;(async function controller() {
