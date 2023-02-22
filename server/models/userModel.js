a8d595f55b1ce34ed03a0a6b70b11f67a9ef8064
santiagomanso <santiagomanso@web.de>
2023-02-22 16:43:51 +0100

    🔧 implemented random img, reworked fields

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,26 +1,49 @@
 import mongoose from 'mongoose'
 import bcrypt from 'bcryptjs'
 
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  isAdmin: {
-    type: Boolean,
-    required: false,
-    default: false,
-  },
-})
+const randomImage = () => {
+  const images = [
+    'https://i.ibb.co/GQDXp1X/aksdasdj-default-user-profile-picture-zombie-style-fun-ux-no-ge-ad402b67-e1fd-482d-b429-a2601634eebe.png',
+    'https://i.ibb.co/wSPJ6WQ/aksdasdj-default-user-profile-girl-zombie-style-c97e338c-dcf6-49e0-a180-111910131ef9-removebg-previe.png',
+  ]
+  return images[Math.floor(Math.random() * images.length)]
+}
 
-const User = mongoose.model('User', userSchema)
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: false,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    image: {
+      type: String,
+      required: false,
+      default: randomImage(),
+    },
+    // favorites:[]
+    // joined date
+    //money spent
+    //ofertas particulares
+    //array favoritos
+  },
+  {
+    timestamps: true,
+  },
+)
+
+const User = mongoose.model('user', userSchema)
 export default User
