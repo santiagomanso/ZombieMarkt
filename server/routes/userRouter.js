97df5c290d20ad3bfb50bfc40c0a571b8e29d3f6
santiagomanso <santiagomanso@web.de>
2023-02-23 23:20:33 +0100

    ⚡️ added controller function to router

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,11 +1,18 @@
 import express from 'express'
-import { createUser, getAllUsers } from '../controllers/userController.js'
+import {
+  createUser,
+  getAllUsers,
+  login,
+} from '../controllers/userController.js'
 import customErrorHandler from '../middleware/customErrorHandler.js'
 
 const userRouter = express.Router()
 
 //CREATE USER
 userRouter.post('/create', customErrorHandler, createUser)
+
+//LOGIN
+userRouter.post('/login', login)
 
 //GET ALL USERS
 userRouter.get('/all', getAllUsers)
