b05d5c7063289964f41e44d9f62ce162f0b3af8a
santiagomanso <santiagomanso@web.de>
2023-02-23 23:21:02 +0100

    🐛 reworked bcrypt, from bcryptJS to bcrypt,

--- server/utils/bcrypt.js
+++ server/utils/bcrypt.js
@@ -1,8 +1,13 @@
-import bcryptjs from 'bcryptjs'
+import bcrypt from 'bcrypt'
 
 export const passwordEncription = async (password) => {
   const saltRounds = 10
-  const salt = await bcryptjs.genSalt(saltRounds)
-  const hashedPassword = await bcryptjs.hash(password, salt)
+  const salt = await bcrypt.genSalt(saltRounds)
+  const hashedPassword = await bcrypt.hash(password, salt)
   return hashedPassword
+}
+
+export const comparePasswords = async (loginPassword, userPassword) => {
+  const result = await bcrypt.compare(loginPassword, userPassword)
+  return result
 }
