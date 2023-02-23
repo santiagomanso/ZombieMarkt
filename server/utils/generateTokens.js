63dbadca98fb3e941e8618b13cffb3b2274d055b
santiagomanso <santiagomanso@web.de>
2023-02-23 23:21:21 +0100

    ⚡️ ✨ utility function generate token

---
+++ server/utils/generateTokens.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const generateToken = (userId) => {
+  const payload = { sub: userId }
+  const options = {
+    expiresIn: '2d',
+    issuer: 'Zombie Markt',
+  }
+
+  const privateKey = process.env.SECRET
+  const token = jwt.sign(payload, privateKey, options)
+  return token
+}
+
+export default generateToken
