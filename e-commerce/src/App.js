b3183df1649511ee9a961e897aa36987a0370e07
santiagomanso <santiagomanso@web.de>
2023-02-23 23:14:11 +0100

    🚚 🌐Added UserContext/AnimationContext new routes

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -5,20 +5,34 @@ import ProductsPage from './pages/Products'
 import HomePage from './pages/Home'
 import Details from './pages/Details'
 import { CartProvider } from './store/CartContext'
+import Login from './pages/Login'
+import AnimationProvider from './store/AnimationContext'
+import Calculator from './pages/Calculator'
+import SurvivalKits from './pages/SurvivalKits'
+import SignUp from './pages/SignUp'
+import UserProvider from './store/UserContext'
 const App = () => {
   return (
-    <CartProvider>
-      <BrowserRouter>
-        <AppContainer>
-          <Navbar />
-          <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/products/:category' element={<ProductsPage />} />
-            <Route path='/products/details/:_id' element={<Details />} />
-          </Routes>
-        </AppContainer>
-      </BrowserRouter>
-    </CartProvider>
+    <UserProvider>
+      <CartProvider>
+        <AnimationProvider>
+          <BrowserRouter>
+            <AppContainer>
+              <Navbar />
+              <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/products/:category' element={<ProductsPage />} />
+                <Route path='/products/details/:_id' element={<Details />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/signup' element={<SignUp />} />
+                <Route path='/calculator' element={<Calculator />} />
+                <Route path='/survivalKits' element={<SurvivalKits />} />
+              </Routes>
+            </AppContainer>
+          </BrowserRouter>
+        </AnimationProvider>
+      </CartProvider>
+    </UserProvider>
   )
 }
 export default App
