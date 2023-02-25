abb009400b39681ff8f00ee4b1b28094cfb1283c
santiagomanso <santiagomanso@web.de>
2023-02-25 20:36:27 +0100

    ⚡️ 🔧 custom hook ready

--- cms/src/hooks/UseFetch.jsx
+++ cms/src/hooks/UseFetch.jsx
@@ -1,7 +1,30 @@
+import axios from 'axios'
 import { useEffect, useState } from 'react'
 
-const UseFetch = () => {
-  const [data, setData] = useState()
+const useFetch = (url) => {
+  const [loading, setLoading] = useState('')
+  const [data, setData] = useState('')
+  const [error, setError] = useState('')
 
-  useEffect(() => {}, [])
+  useEffect(() => {
+    const fetchData = async (url) => {
+      setLoading(true)
+      setTimeout(async () => {
+        try {
+          const { data } = await axios.get(url)
+          if (data) setData(data)
+          setLoading(false)
+        } catch (error) {
+          setError(error)
+          setLoading(false)
+        }
+      }, 500)
+    }
+
+    fetchData(url)
+  }, [url])
+
+  return { loading, data, error }
 }
+
+export default useFetch
