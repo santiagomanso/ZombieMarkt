cba5cccc4b1dd162c1100182c312a50ea0c312cf
santiagomanso <santiagomanso@web.de>
2023-02-28 10:16:21 +0100

    🔧 🎨 reworked css/logic props, rendering

--- e-commerce/src/components/containers/RightContainer.jsx
+++ e-commerce/src/components/containers/RightContainer.jsx
@@ -1,11 +1,17 @@
-const RightContainer = ({ children, gap, relative, overflowHidden }) => {
+const RightContainer = ({
+  children,
+  gap,
+  relative,
+  overflowHidden,
+  justifyCenter,
+}) => {
   return (
     <section
-      className={`flex flex-col items-center lg:bg-black/10 w-full h-full lg:px-10 rounded ${
-        gap ? gap : ''
-      } ${relative ? 'relative' : ''} ${
-        overflowHidden ? overflowHidden : ''
-      }  `}
+      className={`flex flex-col items-center lg:bg-black/10 w-full h-full lg:px-10 rounded
+       ${gap ? gap : ''} ${relative ? 'relative' : ''}
+       ${overflowHidden ? 'overflow-hidden' : ''}
+       ${justifyCenter && 'justify-center'}
+       `}
     >
       {children}
     </section>
