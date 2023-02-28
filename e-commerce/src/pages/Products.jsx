73971600c6464b23600f3e7fca1762ed463c0e8c
santiagomanso <santiagomanso@web.de>
2023-02-28 10:18:20 +0100

    🔧 🎨 reworked logic/ conditional rendering

--- e-commerce/src/pages/Products.jsx
+++ e-commerce/src/pages/Products.jsx
@@ -1,5 +1,4 @@
-import { useNavigate, useParams } from 'react-router-dom'
-import { useState } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import LeftPanel from '../components/leftPanel/LeftPanel'
 import Header from '../components/header/Header'
 import MainContainer from '../components/containers/MainContainer'
@@ -15,6 +14,9 @@ const ProductsPage = () => {
   const url = `http://localhost:5500/api/products/category/${category}`
 
   const { loading, data, error } = useFetch(url)
+  console.log('data', data)
+
+  //FIXME - category params con CMS
 
   return (
     <MainContainer>
@@ -24,24 +26,37 @@ const ProductsPage = () => {
         <Header title={category} typeWritter={false} />
         {loading ? (
           <Loading />
-        ) : (
-          data.products && (
-            <>
-              <ItemList
-                array={data.products}
-                gridOpt='grid-cols-2  lg:grid-cols-3 gap-y-10 lg:gap-y-10 gap-x-5 lg:gap-x-10'
-                itemContainerOpt='lg:w-[340px] h-64'
-                itemImg='w-56 h-56 object-scale-down'
-                redirectToDetail
-              />
+        ) : data.length > 0 || data ? (
+          <>
+            <ItemList
+              array={data.products}
+              gridOpt='grid-cols-2  lg:grid-cols-3 gap-y-10 lg:gap-y-10 gap-x-5 lg:gap-x-10'
+              itemContainerOpt='lg:w-[340px] h-64'
+              itemImg='w-56 h-56 object-scale-down'
+              redirectToDetail
+            />
 
-              <img
-                src={data.products[0].category.image}
-                alt={data.products[0].category.name}
-                className='absolute bottom-0 right-3'
-              />
-            </>
-          )
+            <img
+              src={data.products[0].category.image}
+              alt={data.products[0].category.name}
+              className='absolute bottom-0 right-3'
+            />
+          </>
+        ) : (
+          <article className='flex flex-col items-center gap-2'>
+            <span className='text-3xl'>
+              Uh oh, looks like the zombies got to our{' '}
+              <span className='font-bold text-3xl'>{category}</span> category
+              before we did.
+            </span>
+            <Link
+              to={`http://localhost:3006/create/${category}`}
+              target='_blank'
+              className='text-3xl bg-gradient-to-br from-orange-400/70 to-amber-600/90 rounded flex gap-1 items-baseline outline outline-2 outline-orange-900/40 px-6 py-4 text-gray-200'
+            >
+              Click here to add a new product and help us restock!
+            </Link>
+          </article>
         )}
       </RightContainer>
     </MainContainer>
