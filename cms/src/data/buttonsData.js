0f9fca0647a2a91ad6ef26b4085b8d9de78c502d
santiagomanso <santiagomanso@web.de>
2023-02-28 10:14:04 +0100

    🔧 expanded data for all users/all products page

--- cms/src/data/buttonsData.js
+++ cms/src/data/buttonsData.js
@@ -1,21 +1,32 @@
 export const buttonsData = [
   {
     id: 1,
+    title: 'All Users',
+    path: '/users',
+  },
+  {
+    id: 2,
+    title: 'All products',
+    path: '/products',
+  },
+
+  {
+    id: 3,
     title: 'Create product',
     path: '/create',
   },
   {
-    id: 2,
+    id: 4,
     title: 'Update product',
     path: '/update',
   },
   {
-    id: 3,
+    id: 5,
     title: 'Delete product',
     path: '/delete',
   },
   {
-    id: 4,
+    id: 6,
     title: 'Backup shelves',
     path: '/shelves',
   },
