482a2efb0cde0cd3cefe142578903f3d3bf59d01
santiagomanso <santiagomanso@web.de>
2023-02-28 10:16:40 +0100

    🚧 Working on a custom logo on navbar

--- e-commerce/src/components/dropdown/Dropdown.jsx
+++ e-commerce/src/components/dropdown/Dropdown.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef } from 'react'
 import { Link } from 'react-router-dom'
 
-const Dropdown = ({ name, logout }) => {
+const Dropdown = ({ name, logout, img }) => {
   const [active, setActive] = useState(false)
   const menuRef = useRef()
 
@@ -31,27 +31,36 @@ const Dropdown = ({ name, logout }) => {
     >
       <span>{name}</span>
       <i
-        className={`fa-solid fa-chevron-down ${
-          active ? 'rotate-180' : 'rotate-0'
+        className={`fa-solid fa-chevron-down transition-all ease-in-out duration-500 ${
+          active ? '-rotate-180' : 'rotate-0'
         }`}
       ></i>
 
       <ul
-        className={`absolute top-10 bg-slate-600/70 rounded w-48 flex gap-4 flex-col z-20 ${
-          active ? 'scale-100' : 'scale-0'
+        className={`absolute transition-all ease-in-out duration-150  bg-black/30 rounded w-full left-0 p-2 grid grid-cols-1  z-20 ${
+          active ? ' top-10' : 'scale-0 -top-10'
         }`}
       >
+        <Link
+          to='/profile'
+          className='group p-4 w-full hover:bg-black/60 flex gap-3 items-center '
+        >
+          <div className='w-1/2'>
+            <img src={img} alt={name} className='w-16' />
+          </div>
+          <span className='w-5/6 text-gray-100 text-2xl font-thin tracking-wider'>
+            Profile
+          </span>
+        </Link>
         <li
-          className='group p-2 flex gap-3 items-center'
+          className='group p-4 w-full hover:bg-black/60 flex gap-3 items-center'
           onClick={handleLogout}
         >
-          <i className='fa-solid fa-power-off text-slate-600 group-hover:text-red-600 rotate-180 group-hover:rotate-0 scale-0  group-hover:scale-150 duration-500'></i>
-          <span className=' text-gray-100'>logout</span>
+          <i className='fa-solid pl-4 py-3 w-1/2 fa-power-off text-red-700 text-4xl duration-500'></i>
+          <span className='w-5/6 text-gray-100 text-2xl font-light'>
+            Sign out
+          </span>
         </li>
-        <Link className='group p-2 flex gap-3 items-center '>
-          <i className='fa-solid fa-circle-user text-slate-600 group-hover:text-red-600 rotate-180 group-hover:rotate-0 scale-0  group-hover:scale-150 duration-500'></i>
-          <span className=' text-gray-100'>Profile</span>
-        </Link>
       </ul>
     </div>
   )
