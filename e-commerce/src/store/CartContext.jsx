2e8f929a8873083b3718c099ae5d0162e9b1a85e
santiagomanso <santiagomanso@web.de>
2023-03-02 13:56:14 +0100

    🔥 place order, util function

--- e-commerce/src/store/CartContext.jsx
+++ e-commerce/src/store/CartContext.jsx
@@ -1,4 +1,6 @@
+import axios from 'axios'
 import { createContext, useEffect, useState } from 'react'
+import getTokenFromStorage from '../utils/getTokenFromStorage'
 
 export const CartContext = createContext()
 
@@ -19,6 +21,38 @@ export const CartProvider = ({ children }) => {
     window.localStorage.setItem('cart', JSON.stringify(cart))
   }
 
+  const placeOrder = async (user) => {
+    const token = getTokenFromStorage()
+
+    //NOTE headers
+    const headers = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    }
+
+    const orderIds = []
+    cart.map((item) => {
+      return orderIds.push(item._id)
+    })
+
+    //NOTE body
+    const body = {
+      cart, //the whole array of products
+      shippingAdress: 'Stephan str 60',
+      price: 221,
+      orderItems: orderIds, //array of _ids (only ids)
+    }
+
+    //NOTE axios post with URL, BODY and HEADERS
+    const { data } = axios.post(
+      'http://localhost:5500/api/orders/newOrder',
+      body,
+      headers,
+    )
+  }
+
   useEffect(() => {
     setCartToStorage()
   }, [cart])
@@ -27,6 +61,7 @@ export const CartProvider = ({ children }) => {
   const data = {
     cart,
     setCart,
+    placeOrder,
   }
 
   return <CartContext.Provider value={data}>{children}</CartContext.Provider>
