f2318c4d70859d40e348ed29530128847532a159
santiagomanso <santiagomanso@web.de>
2023-03-02 13:58:10 +0100

    🔐 ⚡️ created custom middle ware AUTH

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/userModel.js'
+
+const authMiddleware = async (req, res, next) => {
+  console.log('req.headers', req.headers)
+  let token
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1]
+      const decoded = jwt.verify(token, process.env.SECRET)
+
+      //insert field into req
+      req.user = await User.findById(decoded.sub).select('-password')
+      next()
+    } catch (error) {
+      res.status(500).json({
+        msg: 'Not authorized, token failed',
+      })
+    }
+  } else {
+    res.status(401).json({
+      msg: 'Not authorized, no token',
+    })
+  }
+}
+
+export { authMiddleware }
