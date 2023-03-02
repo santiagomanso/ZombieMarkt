2be44db4991dec64592eda774ba3717c2730f917
santiagomanso <santiagomanso@web.de>
2023-03-02 13:58:24 +0100

    🔧 reworked order model

--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -1,24 +1,31 @@
 import mongoose from 'mongoose'
 
-const orderSchema = mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'user',
-  },
-  quantity: { type: Number, required: true },
-  price: { type: Number, required: true },
-  orderItems: [
-    {
-      products: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'product',
-      },
+const orderSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'user',
     },
-  ],
-})
+    shippingAdress: {
+      type: String,
+      required: true,
+    },
+    quantity: { type: Number, required: true },
+    price: { type: Number, required: true },
+    orderItems: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: 'product',
+        },
+      },
+    ],
+  },
+  { timestamps: true },
+)
 
-const productModel = mongoose.model('product', orderSchema)
+const orderModel = mongoose.model('order', orderSchema)
 
-export default productModel
+export default orderModel
