df2ca94af20f6a85c9bc97e1002d0c9c12e68bad
santiagomanso <santiagomanso@web.de>
2023-03-02 13:58:39 +0100

    ⚰️ removed random img, moved to controller

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,13 +1,5 @@
 import mongoose from 'mongoose'
 
-const randomImage = () => {
-  const images = [
-    'https://i.ibb.co/GQDXp1X/aksdasdj-default-user-profile-picture-zombie-style-fun-ux-no-ge-ad402b67-e1fd-482d-b429-a2601634eebe.png',
-    'https://i.ibb.co/wSPJ6WQ/aksdasdj-default-user-profile-girl-zombie-style-c97e338c-dcf6-49e0-a180-111910131ef9-removebg-previe.png',
-  ]
-  return images[Math.floor(Math.random() * images.length)]
-}
-
 const userSchema = mongoose.Schema(
   {
     name: {
@@ -31,7 +23,6 @@ const userSchema = mongoose.Schema(
     image: {
       type: String,
       required: false,
-      default: randomImage(),
     },
     // favorites:[]
     // joined date
