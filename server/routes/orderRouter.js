2c1d07bf8bf93f15058354a7f949889f746d978d
santiagomanso <santiagomanso@web.de>
2023-03-06 23:54:32 +0100

    ➕ 🔧 added all order route

--- server/routes/orderRouter.js
+++ server/routes/orderRouter.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import { createOrder } from '../controllers/orderController.js'
+import { createOrder, getAllOrders } from '../controllers/orderController.js'
 import { authMiddleware } from '../middleware/authMiddleware.js'
 
 const orderRouter = express.Router()
 
 //NOTE create order
 orderRouter.post('/newOrder', authMiddleware, createOrder)
+
+//NOTE - Get all orders
+orderRouter.get('/all', getAllOrders)
 
 export default orderRouter
