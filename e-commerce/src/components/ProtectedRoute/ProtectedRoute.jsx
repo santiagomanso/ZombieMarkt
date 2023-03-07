fc2bba8f022dc2fc7f929d9ce36e9742886b9d6d
santiagomanso <santiagomanso@web.de>
2023-03-07 22:07:36 +0100

    🔥 feature redirect msg component

--- e-commerce/src/components/ProtectedRoute/ProtectedRoute.jsx
+++ e-commerce/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,11 +1,12 @@
 import { useContext } from 'react'
 import { UserContext } from '../../store/UserContext'
 import { Navigate } from 'react-router-dom'
+import RedirectMsg from '../../pages/RedirectMsg'
 const ProtectedRoute = ({ children }) => {
   const { user } = useContext(UserContext)
 
   if (!user) {
-    return <Navigate to='/login' />
+    return <RedirectMsg />
   } else return children
 }
 
