e6b2b7011363db24cfcfadd75a6d7bd0be3fb4fa
santiagomanso <santiagomanso@web.de>
2023-03-09 20:33:30 +0100

    ⚡️ 🔧 fetch profile w/header modal/orders

--- e-commerce/src/pages/Profile.jsx
+++ e-commerce/src/pages/Profile.jsx
@@ -1,15 +1,62 @@
-import React, { useContext } from 'react'
+import axios from 'axios'
+import React, { useContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 import MainContainer from '../components/containers/MainContainer'
 import RightContainer from '../components/containers/RightContainer'
 import LeftPanel from '../components/leftPanel/LeftPanel'
+import ModalOrders from '../components/modal/ModalOrders'
+
 import { UserContext } from '../store/UserContext'
+import getTokenFromStorage from '../utils/getTokenFromStorage'
 
 const Profile = () => {
-  const { user } = useContext(UserContext)
+  const { user, setUser } = useContext(UserContext)
+  const navigate = useNavigate()
+
+  const [orders, setOrders] = useState('')
+
+  const [active, setActive] = useState(false)
+
+  useEffect(() => {
+    if (!user) navigate('/')
+    const getProfile = async () => {
+      const myHeaders = {
+        authorization: `Bearer ${getTokenFromStorage()}`,
+      }
+      try {
+        const { data } = await axios.get(
+          'http://localhost:5500/api/users/profile',
+          {
+            headers: myHeaders,
+          },
+        )
+        setUser(data.user)
+        // console.log('user', user)
+      } catch (error) {
+        console.log('error', error)
+      }
+    }
+
+    if (!orders) {
+      getProfile()
+    }
+  }, [])
+
   return (
     <MainContainer>
       <LeftPanel bottomCard />
-      <RightContainer justifyCenter>
+      <RightContainer justifyCenter lgPadding='lg:p-20'>
+        {active ? (
+          <ModalOrders
+            active={active}
+            setActive={setActive}
+            orders={user.orders}
+            email={user.email}
+          />
+        ) : (
+          ''
+        )}
         {user && (
           <article className='w-full shadow-2xl rounded-lg flex outline outline-1 outline-gray-600 '>
             <div className='w-1/3 h-full bg-gradient-to-tl rounded p-10'>
@@ -25,7 +72,7 @@ const Profile = () => {
                   Email
                 </label>
                 <input
-                  // onChange={handleChange}
+                  readOnly={true}
                   className='outline outline-1 outline-gray-300'
                   type='text'
                   name='name'
@@ -37,7 +84,7 @@ const Profile = () => {
                   Image
                 </label>
                 <input
-                  // onChange={handleChange}
+                  readOnly={true}
                   className='outline outline-1 outline-gray-300'
                   type='text'
                   name='sku'
@@ -49,9 +96,9 @@ const Profile = () => {
                   Joined on
                 </label>
                 <input
-                  // onChange={handleChange}
-                  className='outline outline-1 outline-gray-300'
-                  type='date'
+                  readOnly={true}
+                  className='outline outline-1 outline-gray-300 text-gray-200 font-medium'
+                  type='text'
                   name='ean'
                   value={user.joined}
                   disabled
@@ -70,7 +117,10 @@ const Profile = () => {
                 </button>
               </div>
               <div className='flex flex-col lg:col-span-1 w-full'>
-                <button className='flex gap-1 justify-center items-baseline w-full bg-slate-800/60 font-bold text-gray-100 tracking-wider text-lg rounded outline outline-2 outline-slate-500 self-center place-self-center'>
+                <button
+                  onClick={() => setActive(true)}
+                  className='flex gap-1 justify-center items-baseline w-full bg-slate-800/60 font-bold text-gray-100 tracking-wider text-lg rounded outline outline-2 outline-slate-500 self-center place-self-center'
+                >
                   <i className='fa-solid fa-folder-open'></i>
                   <span>My Orders</span>
                 </button>
