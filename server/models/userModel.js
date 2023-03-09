e6ac1882a9472f3d8802deb8ef9b501d531662cb
santiagomanso <santiagomanso@web.de>
2023-03-09 20:38:06 +0100

    🔧 added/joined, moment formated

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,46 +1,45 @@
 import mongoose from 'mongoose'
+import moment from 'moment'
 
-const userSchema = mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: false,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    isAdmin: {
-      type: Boolean,
-      required: false,
-      default: false,
-    },
-    image: {
-      type: String,
-      required: false,
-    },
-    orders: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'order',
-      },
-    ],
-    // favorites:[]
-    // joined date
-    //money spent
-    //ofertas particulares
-    //array favoritos
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: false,
   },
-  {
-    timestamps: true,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
   },
-)
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+  image: {
+    type: String,
+    required: false,
+  },
+  joined: {
+    type: String,
+    default: moment().format('DD-MM-YYYY'),
+  },
+  orders: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'order',
+    },
+  ],
+  // favorites:[]
+  // joined date
+  //money spent
+  //ofertas particulares
+})
 
 const userModel = mongoose.model('user', userSchema)
 export default userModel
