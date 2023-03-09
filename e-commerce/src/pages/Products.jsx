5a6a8c35cff12ebfff67f16aeea6af5f6a7536e7
santiagomanso <santiagomanso@web.de>
2023-03-09 20:32:46 +0100

    🔧 reworked component,  data=>state

--- e-commerce/src/pages/Products.jsx
+++ e-commerce/src/pages/Products.jsx
@@ -6,15 +6,20 @@ import RightContainer from '../components/containers/RightContainer'
 import useFetch from '../hooks/useFetch'
 import ItemList from '../components/lists/ItemList'
 import Loading from '../components/loading/Loading'
+import { useEffect, useState } from 'react'
 
 const ProductsPage = () => {
   const { category } = useParams()
   const navigate = useNavigate()
+  const [products, setProducts] = useState([])
 
   const url = `http://localhost:5500/api/products/category/${category}`
 
   const { loading, data, error } = useFetch(url)
-  console.log('data', data)
+
+  useEffect(() => {
+    if (data) setProducts(data.products)
+  }, [data])
 
   //FIXME - category params con CMS
 
@@ -26,10 +31,10 @@ const ProductsPage = () => {
         <Header title={category} typeWritter={false} />
         {loading ? (
           <Loading />
-        ) : data.length > 0 || data ? (
+        ) : products.length > 0 ? (
           <>
             <ItemList
-              array={data.products}
+              array={products}
               gridOpt='grid-cols-2  lg:grid-cols-3 gap-y-10 lg:gap-y-10 gap-x-5 lg:gap-x-10'
               itemContainerOpt='lg:w-[340px] h-64'
               itemImg='w-56 h-56 object-scale-down'
@@ -37,8 +42,8 @@ const ProductsPage = () => {
             />
 
             <img
-              src={data.products[0].category.image}
-              alt={data.products[0].category.name}
+              src={products[0].category.image}
+              alt={products[0].category.name}
               className='absolute bottom-0 right-3'
             />
           </>
@@ -50,7 +55,7 @@ const ProductsPage = () => {
               before we did.
             </span>
             <Link
-              to={`http://localhost:3006/create/${category}`}
+              to={`http://localhost:3006/create/`}
               target='_blank'
               className='text-3xl bg-gradient-to-br from-orange-400/70 to-amber-600/90 rounded flex gap-1 items-baseline outline outline-2 outline-orange-900/40 px-6 py-4 text-gray-200'
             >
