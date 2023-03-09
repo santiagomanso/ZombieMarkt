95a203d1b3aa6f1b88864867d27be62a0f79508d
santiagomanso <santiagomanso@web.de>
2023-03-09 20:36:25 +0100

    🔧 reworked randomImgs

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,19 +1,24 @@
 import { comparePasswords, passwordEncription } from '../utils/bcrypt.js'
 import generateToken from '../utils/generateTokens.js'
 import jwt from 'jsonwebtoken'
-import orderModel from '../models/orderModel.js'
-import productModel from '../models/productModel.js'
 import userModel from '../models/userModel.js'
 
 //random Image
 const randomImg = () => {
   const images = [
     'https://i.ibb.co/GQDXp1X/aksdasdj-default-user-profile-picture-zombie-style-fun-ux-no-ge-ad402b67-e1fd-482d-b429-a2601634eebe.png',
+    'https://i.ibb.co/GQDXp1X/aksdasdj-default-user-profile-picture-zombie-style-fun-ux-no-ge-ad402b67-e1fd-482d-b429-a2601634eebe.png',
+    'https://i.ibb.co/GQDXp1X/aksdasdj-default-user-profile-picture-zombie-style-fun-ux-no-ge-ad402b67-e1fd-482d-b429-a2601634eebe.png',
+    'https://i.ibb.co/wSPJ6WQ/aksdasdj-default-user-profile-girl-zombie-style-c97e338c-dcf6-49e0-a180-111910131ef9-removebg-previe.png',
     'https://i.ibb.co/wSPJ6WQ/aksdasdj-default-user-profile-girl-zombie-style-c97e338c-dcf6-49e0-a180-111910131ef9-removebg-previe.png',
     'https://i.ibb.co/fFmx31R/basureando-user-profile-stock-picture-cartoon-zombie-style-16f3cc14-3057-4a55-8ee2-2d0fbd9c5538-remo.png',
+    'https://i.ibb.co/fFmx31R/basureando-user-profile-stock-picture-cartoon-zombie-style-16f3cc14-3057-4a55-8ee2-2d0fbd9c5538-remo.png',
     'https://i.ibb.co/2P7XwCp/basureando-user-profile-stock-picture-cartoon-zombie-style-77ddc483-8f05-4aa9-a8ea-c9e4ea39d16e-remo.png',
-    'https://i.ibb.co/pJCC7ZX/basureando-user-profile-picture-cartoon-girl-zombie-style-80557016-2fac-4acc-a431-a7efae327e34-remov.png',
     'https://i.ibb.co/VwWtqKz/basureando-user-profile-picture-cartoon-girl-zombie-style-b46d0887-ac55-4aaf-9c9f-760ab37cb5ba-remov.png',
+    'https://i.ibb.co/VwWtqKz/basureando-user-profile-picture-cartoon-girl-zombie-style-b46d0887-ac55-4aaf-9c9f-760ab37cb5ba-remov.png',
+    'https://i.ibb.co/VwWtqKz/basureando-user-profile-picture-cartoon-girl-zombie-style-b46d0887-ac55-4aaf-9c9f-760ab37cb5ba-remov.png',
+    'https://i.ibb.co/cNGdsQW/basureando-user-profile-picture-cartoon-girl-zombie-style-21ea525f-48a3-424d-8490-e140878f839e-remov.png',
+    'https://i.ibb.co/cNGdsQW/basureando-user-profile-picture-cartoon-girl-zombie-style-21ea525f-48a3-424d-8490-e140878f839e-remov.png',
     'https://i.ibb.co/cNGdsQW/basureando-user-profile-picture-cartoon-girl-zombie-style-21ea525f-48a3-424d-8490-e140878f839e-remov.png',
   ]
   return images[Math.floor(Math.random() * images.length)]
@@ -47,7 +52,7 @@ export const createUser = async (req, res) => {
         password: await passwordEncription(req.body.password), //NOTE bcrypt encription
         image: randomImg(),
       })
-      console.log('newUser', newUser)
+      // console.log('newUser', newUser)
       const savedUser = await newUser.save()
       if (savedUser) {
         const token = generateToken(savedUser._id)
@@ -57,7 +62,7 @@ export const createUser = async (req, res) => {
         const user = {
           email: savedUser.email,
           image: savedUser.image,
-          joined: savedUser.createdAt,
+          joined: savedUser.joined,
         }
         res.status(201).json({
           msg: 'User created successfully',
@@ -67,6 +72,7 @@ export const createUser = async (req, res) => {
       }
     }
   } catch (error) {
+    console.log(error)
     return res.status(500).json({
       msg: 'Fatal Error',
     })
@@ -88,7 +94,13 @@ export const login = async (req, res) => {
 
   try {
     //check if there is a user with the email, if not return a msg and a 404 and RETURN
-    const existingUser = await userModel.findOne({ email: email })
+    const existingUser = await userModel
+      .findOne({ email: email })
+      .populate({
+        path: 'orders',
+        populate: { path: 'orderItems', populate: { path: 'category' } },
+      })
+      .exec()
     if (!existingUser) {
       res.status(404).json({
         msg: `error: email does not exist`,
@@ -108,41 +120,49 @@ export const login = async (req, res) => {
       return
     }
 
+    //FIXME - unselect password
+
     //utility fn generate token
     const token = generateToken(existingUser._id)
     // console.log('token', token)
 
-    //object to return when everything goes OK, (without sending password)
-    const user = {
-      email: existingUser.email,
-      image: existingUser.image,
-      joined: existingUser.createdAt,
-    }
-
     res.status(200).json({
       msg: 'Successfully signed in',
-      user: user,
+      user: existingUser,
       token,
     })
   } catch (error) {
+    console.log(error)
     res.status(500).json({
       msg: 'Fatal error',
     })
   }
 }
 
+// @desc login user and populate orders field array and sub field from order categories
+// @route POST /api/users
+// @access Public
 export const loginWithToken = async (req, res) => {
   const { token } = req.body
   if (token) {
     try {
       const decoded = jwt.verify(token, process.env.SECRET)
-      const user = await userModel.findById(decoded.sub)
+      const user = await userModel
+        .findById(decoded.sub)
+        .populate({
+          path: 'orders',
+          populate: { path: 'orderItems', populate: { path: 'category' } },
+        })
+        .exec()
       if (user) {
         res.status(200).json({
+          //FIXME - unselect password
           user,
         })
       }
-    } catch (error) {}
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
 
@@ -181,25 +201,7 @@ export const getAllUsers = async (req, res) => {
 // @route GET /api/users/profile
 // @access public
 export const getUserProfile = async (req, res) => {
-  // console.log('req.user', req.user)
   res.status(200).json({
     user: req.user,
   })
-  // try {
-  //   //get all users but without password field
-  //   const allUsers = await userModel.find({}).select('-password ')
-  //   if (allUsers)
-  //     res.status(200).json({
-  //       users: allUsers,
-  //     })
-  //   else {
-  //     res.status(500).json({
-  //       msg: 'there was an error fetching all',
-  //     })
-  //   }
-  // } catch (error) {
-  //   res.status(500).json({
-  //     msg: 'Internal server error',
-  //   })
-  // }
 }
