319f885cf805a4c9af961928b0196fea74709a08
santiagomanso <santiagomanso@web.de>
2023-03-13 14:38:33 +0100

    🚧 created config file passport google strategy

---
+++ server/config/googleAuth.js
@@ -0,0 +1,17 @@
+import passport from 'passport'
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20'
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: 'http://www.example.com/auth/google/callback',
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user)
+      })
+    },
+  ),
+)
