7b7d9a4d995702fba2e08402abb72cbce24fdcec
santiagomanso <santiagomanso@web.de>
2023-03-15 17:12:18 +0100

    🔧 newEndpoint-Functions user/login => auth/login

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,8 +3,6 @@ import {
   createUser,
   getAllUsers,
   getUserProfile,
-  login,
-  loginWithToken,
   newFavoriteProduct,
 } from '../controllers/userController.js'
 import { authMiddleware } from '../middleware/authMiddleware.js'
@@ -13,10 +11,8 @@ import customErrorHandler from '../middleware/customErrorHandler.js'
 const userRouter = express.Router()
 
 //POSTS
-//userRouter.get('/google/callback')
 userRouter.post('/create', customErrorHandler, createUser) //new user
-userRouter.post('/login', login) //login email/password
-userRouter.post('/loginWithToken', loginWithToken) //auto login when token
+
 userRouter.post('/newFavoriteProduct/:_id', authMiddleware, newFavoriteProduct) //new user
 
 //GETS
