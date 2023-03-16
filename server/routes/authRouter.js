ef7837b689e82f11c268bb4ca6ca59893a303d20
santiagomanso <santiagomanso@web.de>
2023-03-16 23:11:11 +0100

    🔧reworked router, implemented passport middleware

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -4,12 +4,21 @@ import {
   login,
   loginWithToken,
 } from '../controllers/authController.js'
+import passport from 'passport'
 
 const authRouter = express.Router()
 
 //@desc GOOGLE endpoint (redirect to google) and get accessToken
-//route POST /api/auth/google
-authRouter.post('/google', googleLogin)
+//route GET /api/auth/google
+authRouter.get(
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  }),
+)
+
+authRouter.get('/google/callback', passport.authenticate('google'), googleLogin)
+
 authRouter.post('/login', login) //login email/password
 authRouter.post('/loginWithToken', loginWithToken) //auto login when token
 
