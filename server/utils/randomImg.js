ba1c3b39b9bbb4d4a1b206bf8ab5a1fe69680710
santiagomanso <santiagomanso@web.de>
2023-03-16 23:11:23 +0100

    🔨 created helper function

---
+++ server/utils/randomImg.js
@@ -0,0 +1,20 @@
+//random Image
+export const randomImg = () => {
+  const images = [
+    'https://i.ibb.co/GQDXp1X/aksdasdj-default-user-profile-picture-zombie-style-fun-ux-no-ge-ad402b67-e1fd-482d-b429-a2601634eebe.png',
+    'https://i.ibb.co/GQDXp1X/aksdasdj-default-user-profile-picture-zombie-style-fun-ux-no-ge-ad402b67-e1fd-482d-b429-a2601634eebe.png',
+    'https://i.ibb.co/GQDXp1X/aksdasdj-default-user-profile-picture-zombie-style-fun-ux-no-ge-ad402b67-e1fd-482d-b429-a2601634eebe.png',
+    'https://i.ibb.co/wSPJ6WQ/aksdasdj-default-user-profile-girl-zombie-style-c97e338c-dcf6-49e0-a180-111910131ef9-removebg-previe.png',
+    'https://i.ibb.co/wSPJ6WQ/aksdasdj-default-user-profile-girl-zombie-style-c97e338c-dcf6-49e0-a180-111910131ef9-removebg-previe.png',
+    'https://i.ibb.co/fFmx31R/basureando-user-profile-stock-picture-cartoon-zombie-style-16f3cc14-3057-4a55-8ee2-2d0fbd9c5538-remo.png',
+    'https://i.ibb.co/fFmx31R/basureando-user-profile-stock-picture-cartoon-zombie-style-16f3cc14-3057-4a55-8ee2-2d0fbd9c5538-remo.png',
+    'https://i.ibb.co/2P7XwCp/basureando-user-profile-stock-picture-cartoon-zombie-style-77ddc483-8f05-4aa9-a8ea-c9e4ea39d16e-remo.png',
+    'https://i.ibb.co/VwWtqKz/basureando-user-profile-picture-cartoon-girl-zombie-style-b46d0887-ac55-4aaf-9c9f-760ab37cb5ba-remov.png',
+    'https://i.ibb.co/VwWtqKz/basureando-user-profile-picture-cartoon-girl-zombie-style-b46d0887-ac55-4aaf-9c9f-760ab37cb5ba-remov.png',
+    'https://i.ibb.co/VwWtqKz/basureando-user-profile-picture-cartoon-girl-zombie-style-b46d0887-ac55-4aaf-9c9f-760ab37cb5ba-remov.png',
+    'https://i.ibb.co/cNGdsQW/basureando-user-profile-picture-cartoon-girl-zombie-style-21ea525f-48a3-424d-8490-e140878f839e-remov.png',
+    'https://i.ibb.co/cNGdsQW/basureando-user-profile-picture-cartoon-girl-zombie-style-21ea525f-48a3-424d-8490-e140878f839e-remov.png',
+    'https://i.ibb.co/cNGdsQW/basureando-user-profile-picture-cartoon-girl-zombie-style-21ea525f-48a3-424d-8490-e140878f839e-remov.png',
+  ]
+  return images[Math.floor(Math.random() * images.length)]
+}
