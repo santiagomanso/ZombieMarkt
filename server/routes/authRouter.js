46417bdec6249862c0d69065d0bc32c291142a9f
santiagomanso <santiagomanso@web.de>
2023-03-18 20:21:14 +0100

    🐛fixed bug: passport tried to use express-session

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -10,14 +10,21 @@ const authRouter = express.Router()
 
 //@desc GOOGLE endpoint (redirect to google) and get accessToken
 //route GET /api/auth/google
+//NOTE - session false object on routes otherwise session error
 authRouter.get(
   '/google',
   passport.authenticate('google', {
+    session: false,
     scope: ['profile', 'email'],
   }),
 )
 
-authRouter.get('/google/callback', passport.authenticate('google'), googleLogin)
+//NOTE - session false object on routes otherwise session error
+authRouter.get(
+  '/google/callback',
+  passport.authenticate('google', { session: false }),
+  googleLogin,
+)
 
 authRouter.post('/login', login) //login email/password
 authRouter.post('/loginWithToken', loginWithToken) //auto login when token
