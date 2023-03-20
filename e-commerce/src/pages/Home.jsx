1a635b5ebf4c8b25c68070bb54d31a6c4dc7ed01
santiagomanso <santiagomanso@web.de>
2023-03-20 23:33:41 +0100

    🎨 css improvements responsiveness

--- e-commerce/src/pages/Home.jsx
+++ e-commerce/src/pages/Home.jsx
@@ -20,7 +20,7 @@ const HomePage = () => {
         bottomCard
       />
 
-      <RightContainer gap='lg:gap-32' lgPadding='lg:p-10' overflowAuto>
+      <RightContainer gap='lg:gap-32' padding='lg:p-10' overflowAuto>
         <Header
           title='ZombieMarkt Groceries'
           subtitle='Where prices are dropped dead'
@@ -33,8 +33,8 @@ const HomePage = () => {
             <div className='mt-5 lg:m-0 '>
               <ItemList
                 array={data.categories}
-                gridOpt='grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-y-10 gap-x-5 md:gap-x-20 lg:gap-y-20 lg:gap-x-40 p-10 lg:p-0'
-                itemContainerOpt='w-[300px] h-[150px] md:w-[200px] md:h-[150px] lg:w-[270px] lg:h-[200px] '
+                gridOpt='grid-cols-1  lg:grid-cols-4 gap-y-10 gap-x-5 md:gap-x-20 lg:gap-y-20 lg:gap-x-40 p-10 lg:p-0'
+                itemContainerOpt='w-[300px] h-[150px]  lg:w-[270px] lg:h-[200px] '
                 itemImg='w-32 h-32  lg:w-48 lg:h-48 object-scale-down'
                 redirectToDetail={false}
               />
