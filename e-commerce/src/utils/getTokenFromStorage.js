8219daf71c36deb0a06f3bf4624cf459f8d5a1f5
santiagomanso <santiagomanso@web.de>
2023-03-20 19:47:36 +0100

    ⚡️ get cookie

--- e-commerce/src/utils/getTokenFromStorage.js
+++ e-commerce/src/utils/getTokenFromStorage.js
@@ -1,7 +1,10 @@
+import cookies from 'js-cookie'
 //NOTE only returns token if there is one, else the token will be undefined
 const getTokenFromStorage = () => {
   if (window.localStorage.getItem('token')) {
     return window.localStorage.getItem('token')
+  } else {
+    return cookies.get('token')
   }
 }
 export default getTokenFromStorage
