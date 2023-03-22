5a2a0bf2a456c22c26a771238cf8b707be8b64b6
santiagomanso <santiagomanso@web.de>
2023-03-22 21:06:22 +0100

    ⚗️ test endpoint currentUser⚡️, removed sessions

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import {
+  currentUser,
   googleLogin,
   login,
   loginWithToken,
@@ -14,19 +15,16 @@ const authRouter = express.Router()
 authRouter.get(
   '/google',
   passport.authenticate('google', {
-    session: false,
     scope: ['profile', 'email'],
   }),
 )
 
 //NOTE - session false object on routes otherwise session error
-authRouter.get(
-  '/google/callback',
-  passport.authenticate('google', { session: false }),
-  googleLogin,
-)
+authRouter.get('/google/callback', passport.authenticate('google'), googleLogin)
 
 authRouter.post('/login', login) //login email/password
 authRouter.post('/loginWithToken', loginWithToken) //auto login when token
+
+authRouter.get('/currentUser', currentUser)
 
 export default authRouter
