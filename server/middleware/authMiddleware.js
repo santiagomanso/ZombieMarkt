fe84f2d347cc5544fc5e7062d21b89154555a59b
santiagomanso <santiagomanso@web.de>
2023-03-23 22:21:33 +0100

    🔒️fixed custom middleWare, failed to decode error

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -3,7 +3,7 @@ import userModel from '../models/userModel.js'
 
 const authMiddleware = async (req, res, next) => {
   // console.log('req', req)
-  // console.log(req.headers.authorization)
+  console.log(req.headers.authorization)
   let token
   if (
     req.headers.authorization &&
@@ -12,13 +12,17 @@ const authMiddleware = async (req, res, next) => {
     try {
       token = req.headers.authorization.split(' ')[1]
       const decoded = jwt.verify(token, process.env.SECRET)
-
-      //insert field into req
-      req.user = await userModel
-        .findById(decoded.sub)
-        .select('-password')
-        .exec()
-      next() //calls next method
+      if (decoded) {
+        req.user = await userModel
+          .findById(decoded.sub)
+          .select('-password')
+          .exec()
+        next() //calls next method
+      } else {
+        res.status(500).json({
+          msg: 'Not authorized, token failed',
+        })
+      }
     } catch (error) {
       res.status(500).json({
         msg: 'Not authorized, token failed',
