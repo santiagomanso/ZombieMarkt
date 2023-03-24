0ca092828e3fb604b7df2c8da8c345bf9bcf4326
santiagomanso <santiagomanso@web.de>
2023-03-24 22:27:48 +0100

    🔨 new function get by name

--- server/controllers/categoriesController.js
+++ server/controllers/categoriesController.js
@@ -8,3 +8,19 @@ export const getAllCategories = async (req, res) => {
     })
   } catch (error) {}
 }
+
+export const getCategoryByName = async (req, res) => {
+  try {
+    const category = await categoryModel.findOne({ name: req.params.name })
+    // console.log('category', category)
+    if (category) {
+      res.status(200).json({
+        category: category,
+      })
+    } else {
+      res.status(404).json({
+        msg: 'Category not found',
+      })
+    }
+  } catch (error) {}
+}
