2aa62cce90a1cded979f103f7f62f7ba97fb740b
santiagomanso <santiagomanso@web.de>
2023-03-24 22:33:07 +0100

    🔧 added endpoint to router and function

--- server/routes/categoriesRouter.js
+++ server/routes/categoriesRouter.js
@@ -1,7 +1,11 @@
 import express from 'express'
-import { getAllCategories } from '../controllers/categoriesController.js'
+import {
+  getAllCategories,
+  getCategoryByName,
+} from '../controllers/categoriesController.js'
 const categoriesRouter = express.Router()
 
 categoriesRouter.get('/all', getAllCategories)
+categoriesRouter.get('/:name', getCategoryByName)
 
 export default categoriesRouter
