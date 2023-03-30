b7f7ec21c41253467d633c9580e5a89f88299dd3
santiagomanso <santiagomanso@web.de>
2023-03-30 15:34:34 +0200

    🎨 🔧 moved import css to top

--- e-commerce/src/index.js
+++ e-commerce/src/index.js
@@ -1,7 +1,7 @@
+import '../src/styles/index.css'
 import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import '../src/styles/index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
