71ffac049b1d9e03aa7d97e4f552087e21c67a6a
santiagomanso <santiagomanso@web.de>
2023-03-30 15:40:06 +0200

    ⚗️ css test

--- e-commerce/src/components/containers/AppContainer.jsx
+++ e-commerce/src/components/containers/AppContainer.jsx
@@ -1,6 +1,6 @@
 const AppContainer = ({ children }) => {
   return (
-    <div className='h-screen bg-gradient-to-br from-amber-100 to-slate-900'>
+    <div className='app-container h-screen bg-gradient-to-br from-amber-100 to-slate-900'>
       {children}
     </div>
   )
