d0cc80b94351c467da89f37dc2bf38b9b5fdc5b2
santiagomanso <santiagomanso@web.de>
2023-03-31 16:10:05 +0200

    cookie and redirect methods chained

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -18,11 +18,11 @@ export const googleLogin = async (req, res) => {
 
     //NOTE cookie test with domain to get it to work on vercel
     console.log('req.session', req.session)
-    res.cookie('token', token)
+    res.cookie('token', token).redirect(`${process.env.CLIENT_URL}`)
     // res.send('cookies info')
     console.log('token', token)
     console.log('req.user', req.user)
-    res.redirect(302, `${process.env.CLIENT_URL}`)
+    // res.redirect(`${process.env.CLIENT_URL}`)
   } else {
     res.json({
       msg: 'there is no current user',
