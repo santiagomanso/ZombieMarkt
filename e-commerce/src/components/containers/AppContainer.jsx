e275752297dac6a91dc7ea67fc5aaa1770b5de74
santiagomanso <santiagomanso@web.de>
2023-04-14 18:25:07 +0200

    🔧 conditional splash screen style for component

--- e-commerce/src/components/containers/AppContainer.jsx
+++ e-commerce/src/components/containers/AppContainer.jsx
@@ -1,6 +1,26 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+
 const AppContainer = ({ children }) => {
+  const location = useLocation()
+  const [splash, setSplash] = useState(false)
+  useEffect(() => {
+    if (location.pathname === '/') {
+      setSplash(true)
+    } else {
+      setSplash(false)
+    }
+  }, [location])
+
   return (
-    <div className='app-container h-screen bg-gradient-to-br from-amber-100 to-slate-900'>
+    <div
+      className={`app-container h-screen ${
+        splash
+          ? 'bg-gradient-to-br from-stone-900 to-gray-900 overflow-hidden'
+          : 'bg-gradient-to-br from-amber-100 to-slate-900'
+      }`}
+    >
       {children}
     </div>
   )
