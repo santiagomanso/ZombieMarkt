19e2af8698e3f411553330fe646f135bfd5971de
santiagomanso <santiagomanso@web.de>
2023-07-15 18:03:46 +0200

    🔨 created countries data for language switcher

---
+++ q-commerce/src/utils/countries.js
@@ -0,0 +1,32 @@
+export const countries = [
+  {
+    id: 1,
+    name: {
+      en: 'English',
+      es: 'Ingles',
+      de: 'Englisch',
+    },
+    flag: 'https://cdn3.iconfinder.com/data/icons/142-mini-country-flags-16x16px/32/flag-usa2x.png',
+    language: 'en',
+  },
+  {
+    id: 2,
+    name: {
+      en: 'German',
+      es: 'Aleman',
+      de: 'Deutsch',
+    },
+    flag: 'https://cdn3.iconfinder.com/data/icons/142-mini-country-flags-16x16px/32/flag-germany2x.png',
+    language: 'de',
+  },
+  {
+    id: 3,
+    name: {
+      en: 'Spanish',
+      es: 'Español',
+      de: 'Spanisch',
+    },
+    flag: 'https://cdn3.iconfinder.com/data/icons/142-mini-country-flags-16x16px/32/flag-spain2x.png',
+    language: 'es',
+  },
+]
