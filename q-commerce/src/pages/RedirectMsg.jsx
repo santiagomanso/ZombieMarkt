1830eb65156e3908322a6d86d5c19887757ff1a5
santiagomanso <santiagomanso@web.de>
2023-07-16 17:38:17 +0200

    ✨ added language texts

--- q-commerce/src/pages/RedirectMsg.jsx
+++ q-commerce/src/pages/RedirectMsg.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { LanguageContext } from '../store/LanguageContext'
 
 const RedirectMsg = () => {
   const navigate = useNavigate()
@@ -9,6 +10,8 @@ const RedirectMsg = () => {
   const [animation, setAnimation] = useState(
     'animate__animated animate__bounceIn',
   )
+
+  const { txt } = useContext(LanguageContext)
 
   useEffect(() => {
     timerId.current = setInterval(() => {
@@ -37,13 +40,13 @@ const RedirectMsg = () => {
         <section className='flex flex-col gap-3'>
           <article className='flex gap-2 items-end lg:items-baseline'>
             <span className=' text-amber-800 dark:text-amber-500 text-2xl lg:text-5xl font-semibold'>
-              You need to login first
+              {txt.youNeedToLoginFirst}
             </span>
             <i className='fa-solid fa-lock text-3xl lg:text-5xl text-amber-800 dark:text-amber-500'></i>
           </article>
           <article className=' md:text-2xl flex gap-2 items-center justify-center'>
             <span className='text-amber-800 dark:text-amber-500'>
-              Redirecting to login in {seconds} seconds
+              {txt.redirectingToLogin} {seconds} {txt.seconds}
             </span>
             <i className='fa-solid fa-circle-notch animate-spin text-amber-800 dark:text-amber-500 text-3xl'></i>
           </article>
