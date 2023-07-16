1b11740905c42ef634f9c10d70235ec43a8da4cf
santiagomanso <santiagomanso@web.de>
2023-07-16 17:37:13 +0200

    🎨 improved css bottom position

--- q-commerce/src/components/footer/Footer.jsx
+++ q-commerce/src/components/footer/Footer.jsx
@@ -4,10 +4,10 @@ const Footer = ({ display }) => {
       href='https://github.com/santiagomanso'
       target='_blank'
       rel='noreferrer'
-      className={`fixed bottom-1 left-[50%] -translate-x-[50%] flex justify-center items-baseline gap-2 pt-2 ${
+      className={`fixed bottom-6 w-full lg:bottom-3 lg:w-auto left-[50%] -translate-x-[50%] flex justify-center items-baseline gap-2 pt-2 ${
         display === 'laptop'
           ? 'hidden lg:block'
-          : 'fixed bottom-5 left-[50%] -translate-x-[50%] lg:hidden'
+          : 'fixed left-[50%] -translate-x-[50%] lg:hidden'
       }`}
     >
       <i className='fa-brands fa-github lg:text-lg  text-gray-300'></i>
